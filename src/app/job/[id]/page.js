f0e88684cc96d67e15edc3d9c9f0ae7467db332a
bobjoe400 <cnamattern@gmail.com>
2025-01-30 17:16:15 -0800

    tailwind working, contact info and name displayed

--- src/app/job/[id]/page.js
+++ src/app/job/[id]/page.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
-export default function Job({params}) {
+export default async function Job(props) {
+  const params = await props.params;
   return (
     <div>
       This is more information about my job number: {params?.id}
--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,41 @@
+import fs from 'fs/promises';
 import Link from 'next/link';
+import '../style/global.css';
 
-export default function Home() {
-
-  let jobList = [{
-    id: 1,
-    name: "Raytheon",
-    date: 2004
-  }, {
-    id: 2,
-    name: "Revolution Prep",
-    date: 2007,
-    description: "test"
-  }, {
-    id: 3,
-    name: "Revolution Prep 2.0",
-    date: 2007,
-    description: "test"
-  }];
-
-  let jobHtml = jobList.map((job) => {
-    //logic here
-    return (<li key={job.id}>
-      <span><Link href={`/job/${job.id}`}>{job.name}</Link></span>
-      <span>{job.date}</span>
-    </li>);
-  });
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + '/src/data/jobList.json', 'utf8');
+  
+  const jobList = JSON.parse(file);
 
   return (
-    <div>
-      Hello world;
-      Job List
-      <ul>
-        {jobHtml}
+    <div>    
+      <div className="flex justify-center w-full bg-gray-500 p-4">
+        <span className="text-2xl font-bold">Cooper Mattern</span>
+      </div>
+
+      <div class="flex bg-gray-100 p-2">
+        <div class="flex-1 flex justify-start">cnamatten@gmail.com</div>
+        <div class="flex-1 flex justify-center">github.com/bobjoe400</div>
+        <div class="flex-1 flex justify-end">425-419-6866</div>
+      </div>
+
+      <h2 className="text-xl font-bold">Job List</h2>
+
+      <ul className="list-disc">
+        {jobList.map((job) => (
+          <li key={job.id} className="mb-2">
+            <span>
+              <Link  href={`/job/${job.id}`}>
+                <span className={`
+                  underline
+                  text-blue-500
+                  hover:text-blue-700`
+                }>{job.name}</span>
+              </Link>
+            </span>
+            <span className="ml-2">{job.date}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
